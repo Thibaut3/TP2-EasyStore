c0cc5803627a6a4ab05a01493bf2ac6393cfbacd
ColinBracq <49440144+ColinBracq@users.noreply.github.com>
2021-01-18 13:51:37 +0100

    Update Magasin.h

--- Magasin.h
+++ Magasin.h
@@ -13,18 +13,28 @@ public:
 	void AjoutProduit(std::string titre, std::string description, int quantite, double prix);
 	void AffichageProduits() const;
 	void AffichageProduit(std::string titre) const;
-	void AjoutQuantiteProduit(std::string titre, int quantité); //à n'utiliser que si vous êtes sûr que le produit existe
+	void AjoutQuantiteProduit(std::string titre, int quantité); //à n'utiliser que si vous êtes sûr que le produit existe 
+	void AjoutQuantiteProduit(int IdProduit, int quantité);
 	
 	void AjoutClient(std::string prenom, std::string nom, std::vector<Produit> panier);
 	void AffichageClients() const;
 	void AffichageClient( std::string prenom, std::string nom) const;
-	void AffichageClient(int id);
+	void AffichageClient(int IdClient);
 
-	void AjoutProduit(Produit produit, int Id);
-	void SuppProduit(Produit produit, int Id);
-	void ModifQuantProd(Produit produit, int Id, int quantite);
+	void AjoutProduitPanier(int IdProduit, int IdClient, int quantite);
+	void SuppProduitPanier(int IdProduit, int IdClient);
+	void ModifQuantProdPanier(int IdProduit, int IdClient, int quantite);
+	void ViderPanier(int IdClient);
+	void AffichagePanierClient(int IdClient);
+
+	void ValidationCommande(int Idclient);
+	void MajStatutCommande(int IdCommande, bool statusSouhaite);
+	void AffichageCommandesDemandees();
+	void AffichageCommandeDetail(int idCommande);
+	void AffichageCommandeClient(int idClient);
 
 
+	~Magasin();
 
 private:
 	
