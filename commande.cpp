c1d4d15a6bbcff0fe6d5e7385f25c16c33d892fd
ColinBracq <49440144+ColinBracq@users.noreply.github.com>
2021-01-18 14:47:33 +0100

    Version en débogage (sans les solutions)

--- commande.cpp
+++ commande.cpp
@@ -1,7 +1,7 @@
 #include "commande.h"
 
 //constructeur de la classe Commande
-Commande::Commande(Client client,std::vector<Produit> pdts,bool statut) : _client(client), _pdtCommand(pdts), _statut(statut){}
+Commande::Commande(int id,Client client,std::vector<Produit> pdts,bool statut) : _id(id),_client(client), _pdtsCommand(pdts), _statut(statut){}
 
 //retourne le statut de la commande
 bool Commande::getStatut() const{
@@ -13,11 +13,24 @@ void Commande::setStatut(bool statut){
 	_statut = statut;
 }
 
+Client Commande::getClient() const
+{
+	return _client;
+}
+
+std::string Commande::AffichageSansDetail() const
+{
+	std::string id = std::to_string(_client.getID());
+	std::string size = std::to_string(_pdtsCommand.size());
+	return _id +  " " + _client.getPrenom() + "  " + _client.getNom() + id + "   " + size;
+ 
+}
+
 //surcharge de l'opérateur <<
 std::ostream& operator << (std::ostream &out, const Commande &com){
 	out << com._client.getID() << " " << com._client.getPrenom() << " " << com._client.getNom() << std::endl;
-	for(std::size_t i=0; i < com._pdtCommand.size(); i++){
-		out << com._pdtCommand.at(i).getTitre() << "	" << com._pdtCommand.at(i).getDesc() << "		" << com._pdtCommand.at(i).getPrixU() << std::endl;
+	for(std::size_t i=0; i < com._pdtsCommand.size(); i++){
+		out << com._pdtsCommand.at(i).getTitre() << "	" << com._pdtsCommand.at(i).getDesc() << "		" << com._pdtsCommand.at(i).getPrixU() << std::endl;
 	}
 	if(com._statut == false){
 		out << "\npas livrée" << std::endl;
